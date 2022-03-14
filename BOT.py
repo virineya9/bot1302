ea4cbc9279012bf582aeb9eb8a914d2118e6d029
virineya9 <101557694+virineya9@users.noreply.github.com>
2022-03-14 14:15:19 +0300

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -3,7 +3,8 @@ token = '5209209288:AAE2TcFelXOkvp7GQKOr5Bgh9mbJxEMUe1w'
 bot = telebot.TeleBot(token)
 
 @bot.message_handler(content_types=["text"])
-def repeat_all_messages(message):
-       bot.send_message(message.chat.id, messege.text)
+def repeat_all_messages(message): # Название функции не играет никакой роли
+    bot.send_message(message.chat.id, message.text)
+
 if __name__ == '__main__':
-       bot.infinity_polling()
+     bot.infinity_polling()
