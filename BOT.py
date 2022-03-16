c8084b9e46794ac1aa2c9e545347ec006c663a31
virineya9 <101557694+virineya9@users.noreply.github.com>
2022-03-16 10:07:41 +0300

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -5,19 +5,26 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=["start"])
 def start_message(message):
-    bot.send_message(message.chat.id, text = "Приветик, {0.first_name}!".format(message.from_user))  
+    bot.send_message(message.chat.id, text = "Приветик, {0.first_name}!, я тестовый БОТ".format(message.from_user))  
     
 @bot.message_handler(commands=["button"])
 def button_message(message):
     markup= types.ReplyKeyboardMarkup(resize_keyboard = True)
-    item1 = types.KeyboardButton("Кнопочка")
+    item1 = types.KeyboardButton("Получить мем с котиком")
+    item2 = types.KeyboardButton("Узнать, на что способен, Великий БОТ")
     markup.add(item1)
+    markup.add(item2)
     bot.send_message(message.chat.id, 'Выберите, зачем вы тут', reply_markup = markup)    
 
 @bot.message_handler(content_types=["text"])
 def message_reply(message):
-    if message.text == "Кнопочка":
-        bot.send_photo(message.chat.id, photo = open('p.jpg', 'rb'))     
+    if message.text == "Получить мем с котиком":
+        bot.send_photo(message.chat.id, photo = open('p.jpg', 'rb'))   
+
+@bot.message_handler(content_types=["text"])
+def message_reply(message):
+    if message.text == "Узнать, на что способен, Великий БОТ":
+        bot.send_message(message.chat.id, 'Я могу отправить мем с котиком.', reply_markup = markup)           
     
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли
