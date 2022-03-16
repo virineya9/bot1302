96a10a1a1100c3b1707caf6e8de4d11feeef3969
virineya9 <101557694+virineya9@users.noreply.github.com>
2022-03-16 09:28:27 +0300

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -2,22 +2,15 @@ import telebot
 from telebot import types
 token = '5209209288:AAE2TcFelXOkvp7GQKOr5Bgh9mbJxEMUe1w'
 bot = telebot.TeleBot(token)
-
-@bot.message_handler(commands=["start"])
-def  start_message(message):
-    bot.send_message(message.chat.id, text = "Приветик, {0.first_name}!".format(message.from_user))
     
 @bot.message_handler(commands=["button"])
     def button_message(message):
         markup= types.ReplyKeyboardMarkup(resize_keyboard = True)
         item1 = types.KeyboardButton("Кнопочка")
         markup.add(item1)
-        bot.send_message(message.chat.id, 'Выберите, зачем вы тут', reply_markup = markup)
-        
+        bot.send_message(message.chat.id, 'Выберите, зачем вы тут', reply_markup = markup)    
     
-@bot.message_handler(content_types=["text"])
-def repeat_all_messages(message): # Название функции не играет никакой роли
-    bot.send_message(message.chat.id, message.text)
+
 
 if __name__ == '__main__':
      bot.infinity_polling()
