68a7d5f2507830ab1f3f2656e559078b2dbe79d4
virineya9 <101557694+virineya9@users.noreply.github.com>
2022-03-16 08:41:10 +0300

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -5,6 +5,11 @@ bot = telebot.TeleBot(token)
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли
     bot.send_message(message.chat.id, message.text)
+    
+@bot.message_handler(comands=["start"])
+def  start_message(message):
+    bot.send_message(message.chat.id, text = "Hi, {0.first_name}!".format(message.from_user))
 
 if __name__ == '__main__':
      bot.infinity_polling()
+        
