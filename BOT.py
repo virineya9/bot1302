3427937e767a5c999b8ec40aa137aca98e5c34fe
virineya9 <101557694+virineya9@users.noreply.github.com>
2022-03-16 09:44:23 +0300

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -14,10 +14,9 @@ def button_message(message):
     markup.add(item1)
     bot.send_message(message.chat.id, 'Выберите, зачем вы тут', reply_markup = markup)    
 
-@bot.message_handler(content_types=["text"]) 
-def message_reply(message):
-    if message.text == "Кнопочка":
-        
+@bot.message_handler(commands=["img"]) 
+def image(message):
+    bot.send_photo(message.chat.id, photo = open('kotokdlybotik.jpg,crdownload', 'rb'))     
     
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли
