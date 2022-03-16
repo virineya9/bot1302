beb1e31cdf7d628527aacb6fc0e8de57dcd56cec
virineya9 <101557694+virineya9@users.noreply.github.com>
2022-03-16 08:46:48 +0300

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -2,9 +2,6 @@ import telebot
 token = '5209209288:AAE2TcFelXOkvp7GQKOr5Bgh9mbJxEMUe1w'
 bot = telebot.TeleBot(token)
 
-@bot.message_handler(content_types=["text"])
-def repeat_all_messages(message): # Название функции не играет никакой роли
-    bot.send_message(message.chat.id, message.text)
     
 @bot.message_handler(commands=["start"])
 def  start_message(message):
