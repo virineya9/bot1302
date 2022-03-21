749be2d4b72cf1cf3a0e817b875e37a010d6163d
virineya9 <101557694+virineya9@users.noreply.github.com>
2022-03-21 12:40:36 +0300

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -7,24 +7,28 @@ bot = telebot.TeleBot(token)
 def start_message(message):
     bot.send_message(message.chat.id, text = "Приветик, {0.first_name}!, я тестовый БОТ".format(message.from_user))  
     
+@bot.message_handler(commands=["start"])
+def start_message(message):
+    bot.send_message(message.chat.id, text = "Ты заблудился ночью в лесу. Долго ходил-бродил и искал выход из леса, но ТУТ ты увидел жутко темный, жутко подозрительный и жутко жуткий замок.".format(message.from_user))  
+    
 @bot.message_handler(commands=["button"])
 def button_message(message):
     markup= types.ReplyKeyboardMarkup(resize_keyboard = True)
-    item1 = types.KeyboardButton("Получить мем с котиком")
-    item2 = types.KeyboardButton("Узнать, на что способен, Великий БОТ")
+    item1 = types.KeyboardButton("Войти в замок, я же не трус.")
+    item2 = types.KeyboardButton("Поступить разумно и дальше искать выход из леса.")
     markup.add(item1)
     markup.add(item2)
     bot.send_message(message.chat.id, 'Выберите, зачем вы тут', reply_markup = markup)    
 
 @bot.message_handler(content_types=["text"])
 def message_reply(message):
-    if message.text == "Получить мем с котиком":
-        bot.send_photo(message.chat.id, photo = open('p.jpg', 'rb'))   
+    if message.text == "Войти в замок, я же не трус.":
+         bot.send_message(message.chat.id, 'Я могу отправить мем с котиком.', reply_markup = markup)  
 
 @bot.message_handler(content_types=["text"])
 def message_reply(message):
-    if message.text == "Узнать, на что способен, Великий БОТ":
-        bot.send_message(message.chat.id, 'Я могу отправить мем с котиком.', reply_markup = markup)           
+    if message.text == "Поступить разумно и дальше искать выход из леса.":
+        bot.send_message(message.chat.id, 'Удачи с этим, жалкий трус.', reply_markup = markup)           
     
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли
