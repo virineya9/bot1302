0a4608b4971769dfadfab63f22968a3c808f62b6
virineya9 <101557694+virineya9@users.noreply.github.com>
2022-03-21 12:48:09 +0300

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -8,24 +8,25 @@ bot = telebot.TeleBot(token)
 def start_message(message):
     bot.send_message(message.chat.id, text = "Ты заблудился ночью в лесу. Долго ходил-бродил и искал выход из леса, но ТУТ ты увидел жутко темный, жутко подозрительный и жутко жуткий замок.".format(message.from_user))  
     
-@bot.message_handler(commands=["button"])
+@bot.message_handler(commands=["start"])
 def button_message(message):
     markup= types.ReplyKeyboardMarkup(resize_keyboard = True)
     item1 = types.KeyboardButton("Войти в замок, я же не трус.")
     item2 = types.KeyboardButton("Поступить разумно и дальше искать выход из леса.")
     markup.add(item1)
     markup.add(item2)
-    bot.send_message(message.chat.id, 'Выберите, зачем вы тут', reply_markup = markup)    
+    bot.send_message(message.chat.id, 'Как поступишь?', reply_markup = markup)    
 
 @bot.message_handler(content_types=["text"])
 def message_reply(message):
     if message.text == "Войти в замок, я же не трус.":
-         bot.send_message(message.chat.id, 'Я могу отправить мем с котиком.', reply_markup = markup)  
+         #bot.send_message(message.chat.id, 'Я могу отправить мем с котиком.', reply_markup = markup)  
+         
 
 @bot.message_handler(content_types=["text"])
 def message_reply(message):
     if message.text == "Поступить разумно и дальше искать выход из леса.":
-        bot.send_message(message.chat.id, text = 'Удачи с этим, жалкий трус.', reply_markup = markup)           
+        bot.send_message(message.chat.id, 'Удачи с этим.', reply_markup = markup)           
     
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли
