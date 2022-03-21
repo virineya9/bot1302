2a59193c99be84f8d1ffb6a23fe2fe1c61d9979f
virineya9 <101557694+virineya9@users.noreply.github.com>
2022-03-21 12:51:47 +0300

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -6,7 +6,7 @@ bot = telebot.TeleBot(token)
     
 @bot.message_handler(commands=["start"])
 def start_message(message):
-    bot.send_message(message.chat.id, text = "Ты заблудился ночью в лесу. Долго ходил-бродил и искал выход из леса, но ТУТ ты увидел жутко темный, жутко подозрительный и жутко жуткий замок.".format(message.from_user))  
+    bot.send_message(message.chat.id, text = "Ты заблудился ночью в лесу. Долго ходил-бродил и искал выход из леса, но ТУТ ты увидел жутко темный, жутко подозрительный и жутко жуткий замок.")  
     
 @bot.message_handler(commands=["button"])
 def button_message(message):
